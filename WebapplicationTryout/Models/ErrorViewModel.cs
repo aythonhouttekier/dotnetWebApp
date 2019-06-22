1390c17047ced2b4bb6ed51256832c9d06021438
aythonhouttekier <aython.houttekier@telenet.be>
2019-06-22 14:49:01 +0200

    Add project files.

---
+++ WebapplicationTryout/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebapplicationTryout.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
